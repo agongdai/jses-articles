6438d2810bc91fc66fe741ebfe10209a23f0640e
shaojiang <caishaojiang@gmail.com>
2023-11-07 09:55:22 +0800

    next-js-mdx: render markdown

---
+++ next-js-mdx/src/app/privacy/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+import Privacy from './privacy.mdx';
+
+export default function PrivacyPage() {
+  return (
+    <div className='prose'>
+      <Privacy />
+    </div>
+  );
+}
